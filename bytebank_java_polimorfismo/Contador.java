efff3e7adb35c1dc9abaefca78e02848b4b4608a
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-11 09:28:20 -0600

    Se sobreescribe método getBonificacion usando la connotacion @Override, de igual manera se modifica el comportamiento del método para la clase Contador, esta ves , solo se retorna 200

--- bytebank_java_polimorfismo/Contador.java
+++ bytebank_java_polimorfismo/Contador.java
@@ -1,5 +1,9 @@
 package bytebank_java_polimorfismo;
 //Clase número 10
 public class Contador extends Funcionario {
-	
+	@Override
+	public double getBonificacion() {
+		System.out.println("Ejecutando desde Contador");
+		return 200;
+	}
 }
