bdfefe137a66d8f00008f8a742b10bb9a23976a4
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-11 12:58:42 -0600

    Se crea clase CuentaAhorros al proyecto, se aplica el polimorfismo, implementando la Herencia con la clase madre Cuenta y con la palabra reservada extends, así mismo se implementa constructor de la clase CuentaAhorros y dentro del constructor se usa la palabra reservada super(super llama a cualquier atributo de la clase padre),para mandar a traer los atributos del constructor padre Cuenta

---
+++ bytebank_java_polimorfismo/CuentaAhorros.java
@@ -0,0 +1,10 @@
+package bytebank_java_polimorfismo;
+//clase 14
+public class CuentaAhorros extends Cuenta {
+
+	public CuentaAhorros(int agencia, int numero) {
+		super(agencia, numero);
+		// TODO Auto-generated constructor stub
+	}
+
+}
