b8c2b5021b2599870c9f2565336754279c34cc53
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-11 13:03:08 -0600

    Se crea clase CuentaCorriente al proyecto, se aplica el polimorfismo, implementando la Herencia con la clase madre Cuenta y con la palabra reservada extends, así mismo se implementa constructor de la clase CuentaCorriente y dentro del constructor se usa la palabra reservada super(super llama a cualquier atributo de la clase padre),para mandar a traer los atributos del constructor padre Cuenta (se realiza lo mismo que se implemento en la CuentaAhorros)

---
+++ bytebank_java_polimorfismo/CuentaCorriente.java
@@ -0,0 +1,10 @@
+package bytebank_java_polimorfismo;
+//clase 13
+public class CuentaCorriente extends Cuenta {
+
+	public CuentaCorriente(int agencia, int numero) {
+		super(agencia, numero);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
