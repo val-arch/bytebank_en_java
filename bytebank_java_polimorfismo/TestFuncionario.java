0d3d73d766a2bfee0e533b75c8f3b2d6ffb04dec
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-11 16:55:38 -0600

    se realiza lo mismo que la clase de testeo TestControlBonificacion debido a que convertimos la clase Funcionario en abstracta con la palabra reservada abstract, marca error al instanciar la clase Funcionario, entonces instanciamos Funcionario pero con referencia de Contador o Gerente, sería la unica forma, ejemplo: Funcionario diego = new Contador();

--- bytebank_java_polimorfismo/TestFuncionario.java
+++ bytebank_java_polimorfismo/TestFuncionario.java
@@ -3,7 +3,9 @@ package bytebank_java_polimorfismo;
 //clase numero 5
 public class TestFuncionario {
 	public static void main(String[] args) {
-		Funcionario diego = new Funcionario();
+		Funcionario diego = new Contador();//"Funcionario" Al ser clase abstracta no puede ser instanciada como tal,
+		//al ser abstracta es una representación conceptual pero no puede ser representacion fisica del objeto.
+		//Si quieres una instancia, tienes que darme una representacion fisica o real del objeto. entonces podemos elegir Contador o Gerente en lugar de "Funcionario"
 		diego.setNombre("Diego");
 		diego.setDocumento("444556665");
 		diego.setSalario(2000);
