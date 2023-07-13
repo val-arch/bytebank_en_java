a195b1fef932e8fd87d3e433681407c9aa2d8bff
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-12 23:03:20 -0600

    Se crea la clase SistemaInterno. con atributo privado clave del tipo String, y se crea un metodo de tipo boolean con nombre de autentica(se le pasa de parametros al objeto gerente de la clase gerente) el metodo sirve para saber si la contraseña es verdadera o no. El mismo metodo se duplica, solo cambian los parametros que recibe, ahora para la clase Administrador con referencia de objeto gerente

---
+++ bytebank_java_polimorfismo/SistemaInterno.java
@@ -0,0 +1,33 @@
+package bytebank_java_polimorfismo;
+//clase 17
+public class SistemaInterno {
+	
+	private String clave = "12345";
+	
+	//metodo "autentica" a un funcionario, entonces se le pasa de parametro a un gerente
+	public boolean autentica(Gerente gerente) {
+		//atributo de tipo boolean de nombre "puedeIniciarSesion", que guarda al metodo iniciarSesion perteneciente de la clase Gerente. Y se manda a traer y se guarda en este atributo
+		boolean puedeIniciarSesion = gerente.iniciarSesion(clave);//se manda a traer al metodo y se gurada en esta variable atributo
+		if (puedeIniciarSesion) {//SI es (verdadero)
+			System.out.println("Login exitoso");// imprime, si es VERDADERO
+			return true;//retorna true (verdadero)
+		} else { //si NO (Falso)
+			System.out.println("Error en Login");//imprime, si es FALSO
+			return false;//retorna false (falso)
+		}
+	}
+	
+	//metodo "autentica" a un funcionario, entonces se le pasa de parametro a un gerente
+		public boolean autentica(Administrador gerente) {
+			//atributo de tipo boolean de nombre "puedeIniciarSesion", que guarda al metodo iniciarSesion perteneciente de la clase Gerente. Y se manda a traer y se guarda en este atributo
+			boolean puedeIniciarSesion = gerente.iniciarSesion(clave);//se manda a traer al metodo y se gurada en esta variable atributo
+			if (puedeIniciarSesion) {//SI es (verdadero)
+				System.out.println("Login exitoso");// imprime, si es VERDADERO
+				return true;//retorna true (verdadero)
+			} else { //si NO (Falso)
+				System.out.println("Error en Login");//imprime, si es FALSO
+				return false;//retorna false (falso)
+			}
+		}
+	
+}
