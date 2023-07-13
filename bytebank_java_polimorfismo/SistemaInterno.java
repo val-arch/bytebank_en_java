daf67abd2efa80c98e36e1d1b13f7a496bdd868a
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-13 00:43:06 -0600

    Se crea la clase SistemaInterno, se duplica el atributo private String SistemaInterno, que tenian tanto la clase Administrador como Gerente. Se crea metodo public boolean autentica(FuncionarioAutenticable gerente) el cual servira para saber si la clave se logea o no, tanto para Gerente o Administrador

--- bytebank_java_polimorfismo/SistemaInterno.java
+++ bytebank_java_polimorfismo/SistemaInterno.java
@@ -2,10 +2,10 @@ package bytebank_java_polimorfismo;
 //clase 17
 public class SistemaInterno {
 	
-	private String clave = "12345";
+	private String clave = "AluraCursosOnLine";
 	
 	//metodo "autentica" a un funcionario, entonces se le pasa de parametro a un gerente
-	public boolean autentica(Gerente gerente) {
+	public boolean autentica(FuncionarioAutenticable gerente) {
 		//atributo de tipo boolean de nombre "puedeIniciarSesion", que guarda al metodo iniciarSesion perteneciente de la clase Gerente. Y se manda a traer y se guarda en este atributo
 		boolean puedeIniciarSesion = gerente.iniciarSesion(clave);//se manda a traer al metodo y se gurada en esta variable atributo
 		if (puedeIniciarSesion) {//SI es (verdadero)
@@ -16,18 +16,5 @@ public class SistemaInterno {
 			return false;//retorna false (falso)
 		}
 	}
-	
-	//metodo "autentica" a un funcionario, entonces se le pasa de parametro a un gerente
-		public boolean autentica(Administrador gerente) {
-			//atributo de tipo boolean de nombre "puedeIniciarSesion", que guarda al metodo iniciarSesion perteneciente de la clase Gerente. Y se manda a traer y se guarda en este atributo
-			boolean puedeIniciarSesion = gerente.iniciarSesion(clave);//se manda a traer al metodo y se gurada en esta variable atributo
-			if (puedeIniciarSesion) {//SI es (verdadero)
-				System.out.println("Login exitoso");// imprime, si es VERDADERO
-				return true;//retorna true (verdadero)
-			} else { //si NO (Falso)
-				System.out.println("Error en Login");//imprime, si es FALSO
-				return false;//retorna false (falso)
-			}
-		}
-	
+		
 }
