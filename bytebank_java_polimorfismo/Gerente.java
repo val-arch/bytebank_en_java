d88d38367e7dd0ae10fa3465ef24ffa45a011f65
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-13 00:32:05 -0600

    Cambios a la clase Gerente, cambia de clase a la que extendia (heredaba), antes era Funcionario, ahora es FuncionarioAutenticable. se queda solamente con el metodo getBonificacion, sin atributos (se eliminaron los atributos y metodos que tenia)

--- bytebank_java_polimorfismo/Gerente.java
+++ bytebank_java_polimorfismo/Gerente.java
@@ -1,27 +1,13 @@
 package bytebank_java_polimorfismo;
 //sobre-Escritura de metodos es un concepto importante de HERENCIA
 //Clase número 4 - comienza la HERENCIA. Palabra Reservada "extends"
-public class Gerente extends Funcionario {
-	
-	private String clave;
-
-	/*public String getClave() {
-		return clave;
-	}*/
-
-	public void setClave(String clave) {
-		this.clave = clave;
-	}
-	
-	public boolean iniciarSesion(String clave) {
-		return clave == "AluraCursosOnLine";
-	}
-	
+public class Gerente extends FuncionarioAutenticable {
+		
 	// Sobreescritura de metodo (Proviene de la clase Funcionario)
 	public double getBonificacion() {//double
 		System.out.println("Ejecutando desde GERENTE");
 		//modificamos debido a que el metodo se convirtio en abstract
-		return super.getSalario() +	this.getSalario() * 0.5;
+		return 2000;
 	}
 	
 }
