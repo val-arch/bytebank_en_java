44c10086a2c0d529633c0abe19c2001b1931c20e
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-14 16:58:17 -0600

    Se crea esta nueva clase de nombre AutenticableUtil. La cual servirá, para alojar aquí, el atributo: -private String clave- y los 2 métodos ya implementados: -setClave- e -iniciarSesion- -los métodos provienen de la Interface Autenticable, pero se modificará probablemente. El motivo de la creación de esta clase, se debe a que estos métodos se estarán ocupando recurrentemente, por lo tanto se independizarán, y pertenecerán a esta clase

---
+++ bytebank_java_polimorfismo/AutenticacionUtil.java
@@ -0,0 +1,14 @@
+package bytebank_java_polimorfismo;
+
+public class AutenticacionUtil {
+	
+	private String clave;
+	
+	public boolean iniciarSesion(String clave) {
+		return this.clave == clave;
+	}
+	
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+}
