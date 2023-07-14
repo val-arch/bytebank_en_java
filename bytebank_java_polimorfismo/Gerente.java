bfe24aa00dc23e169cfe1d6fb9ebb7347f315d97
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-13 22:03:23 -0600

    La clase Gerente se modifica. ahora extiende de la clase abstracta Funcionario e implementa de la interface Autenticable. solo cuenta con su metodo getBonificacion y se implementan 2 metodos setClave e iniciarSesion con la anotacion @Override

--- bytebank_java_polimorfismo/Gerente.java
+++ bytebank_java_polimorfismo/Gerente.java
@@ -1,13 +1,26 @@
 package bytebank_java_polimorfismo;
 //sobre-Escritura de metodos es un concepto importante de HERENCIA
 //Clase número 4 - comienza la HERENCIA. Palabra Reservada "extends"
-public class Gerente extends FuncionarioAutenticable {
+//se aplica lo mismo que en la clase Administrador. Gerente extiende de Funcionario e implementa de Autenticable y tambien implementa sus metodos
+public class Gerente extends Funcionario implements Autenticable {
 		
 	// Sobreescritura de metodo (Proviene de la clase Funcionario)
 	public double getBonificacion() {//double
 		System.out.println("Ejecutando desde GERENTE");
 		//modificamos debido a que el metodo se convirtio en abstract
 		return 2000;
+	}
+
+	@Override
+	public void setClave(String clave) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean iniciarSesion(String clave) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
