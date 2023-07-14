929946980a47b9b3334540e98f1113bf8af6ea1e
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-13 21:59:11 -0600

    La clase FuncionarioAutenticable, se modifica para pasar a llamarse Autenticable y se convierte ahora en una interface. se eliminan sus atributos y solo se dejan sus 2 metodos sin implementar y sin cuerpo

---
+++ bytebank_java_polimorfismo/Autenticable.java
@@ -0,0 +1,19 @@
+package bytebank_java_polimorfismo;
+//clase 18
+//se elimina la palabra reserva "abstract" (por buenas practicas) del nombre de la interface Autenticable, deido a que todas las "interfaces" por default son abstractas y ya viene implicito
+//antes estaba así: public abstract interface Autenticable extends Funcionario { // ahora queda así: public abstract interface Autenticable extends Funcionario {
+//nota: una interfaz no puede extender de una clase, solo de otra interfaz
+public interface Autenticable {
+	//ahora todo (FuncionarioAutenticable)funcionario autenticable , va a tener una clave y va a poder iniciar serión
+	
+	//la interface no puede tener atributos privados, debido a que sus métodos son abstractos de manera implicita y no tienen cuerpo.
+	//y como los metodos no tienen cuerpo, no hay manera de acceder a ellos desde otra clase.
+	//entonces eliminamos el atributo //private String clave;
+	
+
+	//todos los métodos de una interfaz son abstractos
+	public void setClave(String clave);
+	
+	public boolean iniciarSesion(String clave);
+
+}
