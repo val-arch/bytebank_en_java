c831eba7c706a4496d698bbe866c684f4ef224a7
Luis Valdos <luisvaldosabundez@gmail.com>
2023-07-13 21:52:31 -0600

    Se modifica clase Cliente, implementando de la interfaz Autenticable y obligados a implementar los metodos iniciarSesion y setClave sobreescribiendolos con la anotacion @Override

--- bytebank_java_polimorfismo/Cliente.java
+++ bytebank_java_polimorfismo/Cliente.java
@@ -1,6 +1,13 @@
 package bytebank_java_polimorfismo;
 //clase 12
-public class Cliente {
+//una clase no puede extender de algo que no es una clase(como la interfaz Autenticable). 
+//No puede extender de Autenticable, pero si la puede "implementar" Autenticable
+//antes: public class Cliente extends Autenticable {//nuestra clase Cliente se vuelve autenticable extendiendo de la clase Autenticable
+//ahora: public class Cliente implements Autenticable {
+//y al implementar la interface Autenticable, estamos obligados a implementar los metodos(setClave e iniciarSesion se sobreescriben con anotación @Override)
+
+//nota: la clase cliente, ya no tiene nada que ver con la clase Funcionario
+public class Cliente implements Autenticable {//nuestra clase Cliente se vuelve autenticable extendiendo de la clase Autenticable
 	private String nombre;
 	private String documento;
 	private String telefono;
@@ -29,4 +36,17 @@ public class Cliente {
 		this.telefono = telefono;
 	}
 
+
+	@Override
+	public boolean iniciarSesion(String clave) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void setClave(String clave) {
+		// TODO Auto-generated method stub
+	}	
+	
+	
 }
